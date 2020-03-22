faf96b9200e130d902351eacf19a422a16dbb40f
Michael Kunc <mjkunc@gmail.com>
2020-03-22 15:05:25 -0500

    csv writer complete

--- simple_data_dictionary/snowflake_fetcher.py
+++ simple_data_dictionary/snowflake_fetcher.py
@@ -1,6 +1,7 @@
 import os
+from typing import List
 
-# from pathlib import Path
+from pathlib import Path
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
@@ -18,7 +19,7 @@ def get_tables(connection: Engine) -> pd.DataFrame:
     connection.execute(f"USE WAREHOUSE {warehouse};")
     query = (
         "SELECT TABLE_CATALOG, TABLE_SCHEMA, "
-        "contact(TABLE_CATALOG,'_', TABLE_SCHEMA) as SCHEMA_ID, "
+        "concat(TABLE_CATALOG,'_', TABLE_SCHEMA) as SCHEMA_ID, "
         "TABLE_NAME, concat(schema_id,'_',TABLE_NAME) as TABLE_ID, "
         "ROW_COUNT, CREATED, LAST_ALTERED "
         "FROM information_schema.TABLES "
@@ -34,32 +35,37 @@ def get_tables(connection: Engine) -> pd.DataFrame:
 
 def db_df(df: pd.DataFrame) -> pd.DataFrame:
     """transforms the query result df into a df for databases"""
-    df = df[["table_catalog"]].drop_duplicates("table_catalog")
-    df["Id:ID"] = df["table_catalog"]
-    df["name"] = df["Id:ID"]
-    df[":LABEL"] = "database"
-    df.drop(columns=["table_catalog"], inplace=True)
-    return df
+    local_df = df.copy(deep=True)
+    local_df = local_df[["table_catalog"]].drop_duplicates("table_catalog")
+    local_df["Id:ID"] = local_df["table_catalog"]
+    local_df["name"] = local_df["Id:ID"]
+    local_df[":LABEL"] = "database"
+    local_df.drop(columns=["table_catalog"], inplace=True)
+    return local_df
 
 
 def schema_df(df: pd.DataFrame) -> pd.DataFrame:
     """transforms the query result into a df for schemas"""
-    df = df[["schema_id", "table_schema"]].drop_duplicates("schema_id")
-    df["Id:ID"] = df["schema_id"]
-    df["name"] = df["table_schema"]
-    df[":LABEL"] = "schema"
-    df.drop(columns=["schema_id", "table_schema"], inplace=True)
-    return df
+    local_df = df.copy(deep=True)
+    local_df = local_df[["schema_id", "table_schema"]].drop_duplicates("schema_id")
+    local_df["Id:ID"] = local_df["schema_id"]
+    local_df["name"] = local_df["table_schema"]
+    local_df[":LABEL"] = "schema"
+    local_df.drop(columns=["schema_id", "table_schema"], inplace=True)
+    return local_df
 
 
 def table_df(df: pd.DataFrame) -> pd.DataFrame:
     # TODO: add the datetime stuff.
-    df = df[["table_catalog", "table_schema", "table_name", "table_id", "row_count"]]
-    df["Id:ID"] = df["table_id"]
-    df["name"] = df["table_name"]
-    df[":LABEL"] = "table"
-    df["ROW_COUNT"] = df["row_count"]
-    df.drop(
+    local_df = df.copy(deep=True)
+    local_df = local_df[
+        ["table_catalog", "table_schema", "table_name", "table_id", "row_count"]
+    ]
+    local_df["Id:ID"] = local_df["table_id"]
+    local_df["name"] = local_df["table_name"]
+    local_df[":LABEL"] = "table"
+    local_df["ROW_COUNT"] = local_df["row_count"]
+    local_df.drop(
         columns=[
             "table_catalog",
             "table_schema",
@@ -69,15 +75,16 @@ def table_df(df: pd.DataFrame) -> pd.DataFrame:
         ],
         inplace=True,
     )
-    return df
+    return local_df
 
 
 def schema_rels_df(df: pd.DataFrame) -> pd.DataFrame:
     """creates a dataframe of the relationships between schemas and databases"""
-    df[":START_ID"] = df["table_catalog"]
-    df[":END_ID"] = df["table_schema"]
-    df[":TYPE"] = df.apply(lambda row: "BELONGS_TO", axis=1)
-    df.drop(
+    local_df = df.copy(deep=True)
+    local_df[":START_ID"] = local_df["table_catalog"]
+    local_df[":END_ID"] = local_df["table_schema"]
+    local_df[":TYPE"] = local_df.apply(lambda row: "BELONGS_TO", axis=1)
+    local_df.drop(
         columns=[
             "table_catalog",
             "table_schema",
@@ -88,15 +95,16 @@ def schema_rels_df(df: pd.DataFrame) -> pd.DataFrame:
         ],
         inplace=True,
     )
-    df.drop_duplicates(":END_ID", inplace=True)
-    return df
+    local_df.drop_duplicates(":END_ID", inplace=True)
+    return local_df
 
 
 def table_rels_df(df: pd.DataFrame) -> pd.DataFrame:
-    df[":START_ID"] = df["schema_id"]
-    df[":END_ID"] = df["table_id"]
-    df[":TYPE"] = df.apply(lambda row: "BELONGS_TO", axis=1)
-    df.drop(
+    local_df = df.copy(deep=True)
+    local_df[":START_ID"] = local_df["schema_id"]
+    local_df[":END_ID"] = local_df["table_id"]
+    local_df[":TYPE"] = local_df.apply(lambda row: "BELONGS_TO", axis=1)
+    local_df.drop(
         columns=[
             "table_catalog",
             "table_schema",
@@ -107,13 +115,32 @@ def table_rels_df(df: pd.DataFrame) -> pd.DataFrame:
         ],
         inplace=True,
     )
-    df.drop_duplicates(":END_ID", inplace=True)
-    return df
+    local_df.drop_duplicates(":END_ID", inplace=True)
+    return local_df
+
+
+def write_dfs_to_csv(dfs: List[pd.DataFrame]) -> None:
+    # TODO: write a check on the length of the dataframe
+    file_names = [
+        "dbs.csv",
+        "schemas.csv",
+        "tables.csv",
+        "schema_rels.csv",
+        "table_rels.csv",
+    ]
+    csv_path = Path(f"{Path.cwd()}/csvs")
+    for ix, df in enumerate(dfs):
+        df.to_csv(path_or_buf=Path(f"{csv_path}/{file_names[ix]}"), index=False)
 
 
 def main():
     engine = create_engine(f"snowflake://{user}:{password}@{account}/")
     connection = engine.connect()
+    dfs = []
     df = get_tables(connection)
-    schema_rels = schema_rels_df(df)
-    print(schema_rels)
+    dfs.append(db_df(df))
+    dfs.append(schema_df(df))
+    dfs.append(table_df(df))
+    dfs.append(schema_rels_df(df))
+    dfs.append(table_rels_df(df))
+    write_dfs_to_csv(dfs)
